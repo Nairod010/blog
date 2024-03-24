5bd37c31e737463238a970c193be34fb384f5a0d
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-03-24 13:02:26 +0200

    made the migration functional because I am a dumbfuck

--- blogdb/models/users.go
+++ blogdb/models/users.go
@@ -1,8 +1,7 @@
 package models
 
 import (
-	"time"
-	
+		
 	"github.com/satori/go.uuid"	
 	"blogdb/db"
 	"gorm.io/gorm"
@@ -27,8 +26,8 @@ type BlogPost struct {
 	Content	string `gorm:"type:text"`
 	AuthorID uuid.UUID `gorm:"type:uuid"`
 	Author User `gorm:"foreignKey:AuthorID"`
-	Categories []Category `gorm:"many2many:post_categories"`
-	Tags []Tag `gorm:"many2many:post_tags"`
+//	Categories []Category `gorm:"many2many:post_categories"`
+	Tags []*Tag `gorm:"many2many:post_tags;foreignKey:ID;joinForeignKey:TagID"`
 }
 
 type Comment struct {
@@ -37,33 +36,43 @@ type Comment struct {
 	Post BlogPost	`gorm:"foreignKey:PostID"`
 	AuthorID string `gorm:"type:uuid"`
 	Author User	`gorm:"foreignKey:AuthorID"`
-	CommentText string `gorm:"type:text"`
-	CommentDate time.Time `gorm::"autoCreateTime;type:timestamp with time zone"`
+	CommentText string `gorm:"type:text"`	
 }
 
 type WikiPage struct {
 	gorm.Model
 	Title string `gorm:"type:varchar(255);"`
 	Content string `gorm:"type:text;"`
-	AuthorID string `gorm:"type:uuid"`
+	AuthorID uuid.UUID `gorm:"type:uuid"`
 	Author User	`gorm:"foreignKey:AuthorID"`
 	Tag []Tag `gorm:"many2many:page_tags"`
+	Categories []*Category `gorm:"many2many:wiki_page_categories"`
 }
 
 type Category struct {
 	gorm.Model
 	Name string `gorm:"type:varchar(100);"`
-	Posts []BlogPost `gorm:"many2many:post_categories"`
+	Posts []*WikiPage `gorm:"many2many:wiki_page_categories"`
 }
 
 type Tag  struct {
 	gorm.Model
 	Name string `gorm:"type:varchar(100)"`
-	BlogPost []BlogPost `gorm:"many2many:post_tags"`
-	WikiPage []WikiPage `gorm:"many2many:page_tags"`
+	BlogPost []*BlogPost `gorm:"many2many:post_tags;foreignKey:ID;joinForeignKey:PostID"`
+	WikiPage []*WikiPage `gorm:"many2many:page_tags"`
 }
 
+type PostTag struct {
+	PostID uuid.UUID `gorm:"type:uuid"`
+	TagID uint `gorm:"type:bigint"`
+}
+
+func (PostTag) TableName() string {
+	return "post_tags"
+}
+
+
 func AutoMigrate() error {
-	return db.DB.AutoMigrate(&User{},&BlogPost{}, &Comment{},&WikiPage{},&Category{},&Tag{})
+	return db.DB.AutoMigrate(&User{},&BlogPost{}, &Comment{},&WikiPage{},&Tag{},&PostTag{})
 }
 
